4b10578a0c49c0e6e6a5e4a8d7d12012382a6d8a
Luiz Oliveira <ziuloliveira@gmail.com>
2015-08-09 00:21:31 -0300

    Initial layout and sample
    Signed-off-by: Luiz Oliveira <ziuloliveira@gmail.com>
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- src/beautify.py
+++ src/beautify.py
@@ -1,1 +1,31 @@
 # -*- coding: utf-8 -*-
+from sys import stderr
+import unittest
+
+
+class TestBeautify(object):
+
+    name = ''
+    module = ''
+
+    def getName(self):
+        """ Get the name of the test """
+        self.name = str(self.id).split('=')[-1][:-2]
+        self.name = self.name.split('test_')[-1]
+        self.name = self.name.replace('_', ' ')
+
+    def __str__(self):
+        self.getName()
+        out = '\r[%s] %s test ' % (self.module, self.name)
+        out = out.ljust(70, '-')
+        return out + ' '
+
+    def tearDown(self):
+        stderr.write(' Done\n')
+
+    def shortDescription(self):
+        return "Test from class %s" % self.__class__.__name__
+
+    def setUp(self):
+        stderr.write(self.__str__())
+        self.shortDescription()
