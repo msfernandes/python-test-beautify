c0179eb913f5a6c1175cd0711340637a4b27a3f3
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2015-08-09 01:21:43 -0300

    Add color
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- src/beautify.py
+++ src/beautify.py
@@ -4,11 +4,36 @@ from re import findall
 from unittest import TestCase
 
 
+class Color(object):
+
+    ngree_color = '\033[01;32m'
+    red_color = '\033[01;31m'
+    white_color = '\033[01;37m'
+    blue_color = '\033[01;94m'
+    default_color = '\033[00;39m'
+
+    @classmethod
+    def green(cls, text):
+        return "%s%s%s" % (cls.green_color, text, cls.default_color)
+
+    @classmethod
+    def red(cls, text):
+        return "%s%s%s" % (cls.red_color, text, cls.default_color)
+
+    @classmethod
+    def white(cls, text):
+        return "%s%s%s" % (cls.white_color, text, cls.default_color)
+
+    @classmethod
+    def blue(cls, text):
+        return "%s%s%s" % (cls.blue_color, text, cls.default_color)
+
+
 class TestBeautify(TestCase):
 
     name = ''
     module = ''
-    distance = 70
+    distance = 100
 
     def get_name(self):
         """ Get the name of the test """
@@ -18,13 +43,15 @@ class TestBeautify(TestCase):
 
     def __str__(self):
         self.get_name()
-        if self.module is not '':
-            out = '\r[%s] %s test ' % (self.module, self.method_name)
-        else:
+        if self.module is '':
             self.module = findall('[A-Z][a-z]*', self.__class__.__name__)
             self.module = ' '.join(self.module)
             out = '\r[%s] %s test ' % (self.module, self.method_name)
-        out = out.ljust(self.distance, '-')
+
+        module = "%s%s%s" % ('[', self.module, ']')
+        method_name = "%s%s" % (self.method_name.capitalize(), 'Test')
+        out = "\r%s %s " % (Color.blue(module), Color.white(method_name))
+        out = out.ljust(self.distance, '_')
         return out + ' '
 
     def run(self, result=None):
